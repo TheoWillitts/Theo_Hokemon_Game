b0c1bb9b63741a16fc20dc529ceb3949ef8e38ea
ThatPandaMan <theowillitts123@gmail.com>
2021-01-14 10:29:29 +0000

    Add project files.

---
+++ Hokemon_T/Hokemon.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon_T
+{
+    class Hokemon
+    {
+        // MEMBERS - All Attributes, MEthods and Properties are Members of this class.
+        // VARIABLES a.k.a ATTRIBUTES inside a class can also be referred to as fields.
+
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defense;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value;  }
+        }
+
+        // Methods
+
+        public Hokemon() //Constructor Method
+        {
+            provide_name();
+            max_health = 100;
+            health = random_int_generator(10, max_health);
+            attack = random_int_generator(10, max_health);
+            speed = random_int_generator(10, max_health);
+            defense = random_int_generator(10, max_health);
+
+        }
+
+        public void provide_name()
+        {
+            Console.WriteLine("Enter a name for your hokemon: ");
+            name = Console.ReadLine();
+        }
+
+        public void get_details()
+        {
+            Console.WriteLine("\n-----------------");
+            Console.WriteLine("\n\nName: {0}\nHealth: {1}/{2}\nAttack: {3}\nSpeed: {4}\nDefense: {5}", Name, Health, max_health, attack, speed, defense);
+            Console.WriteLine("\n\n-----------------");
+        }
+
+
+        public int random_int_generator(int min_value, int max_value)
+        {
+            Random rnd = new Random();
+
+            int randomValue;
+
+            randomValue = rnd.Next(min_value, max_value);
+
+            return randomValue;
+        }
+    }
+}
---
+++ Hokemon_T/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hokemon_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the world of Hokemon!");
+
+            // INSTANTIATION of first object
+
+            Hokemon HokeObject01 = new Hokemon();
+
+            HokeObject01.get_details();
+
+            //Time wait
+            System.Threading.Thread.Sleep(1000); // Sleep for one second.
+            
+            //Creating second Hokemon
+
+            Hokemon HokeObject02 = new Hokemon();
+
+            HokeObject02.get_details();
+        }
+    }
+}
